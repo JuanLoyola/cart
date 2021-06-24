f346ae52a1b3ade6b6581b51c8f6acd9a4cc17d5
Loyola <juanloyola@hotmail.com>
2021-06-24 14:25:30 -0300

    front commit

---
+++ src/components/product/Product_List_Item.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="card">
+      <div class="card-content">
+        <div class="content">
+          <h4>{{ productItem.title }}</h4>
+          <a
+            class="button is-rounded is-link"
+            @click="addCartItem(productItem)"
+          >
+            <strong>Add to Cart</strong>
+          </a>
+          <br />
+          <p class="mt-4">
+            {{ productItem.description }}
+          </p>
+        </div>
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-6">{{ productItem.owner }}</p>
+          </div>
+          <div class="media-right">
+            <a class="button is-primary">
+              <strong>$ {{ productItem.price }}</strong>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+<script>
+import {mapActions} from 'vuex'
+export default {
+  name: "ProductListItem",
+  props: ["productItem"],
+  methods: {
+    ...mapActions(["addCartItem"]),
+  },
+};
+</script>
